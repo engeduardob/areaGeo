df6a0477d34b088f51f67a30f5f0de893296a282
engeduardob <seduardob@hotmail.com>
2024-03-09 16:03:44 -0300

    Adicionada função para area da circufenrência

--- areaGeo.py
+++ areaGeo.py
@@ -1,6 +1,9 @@
-#Esse programa é um teste
-
+import math
 
 def area_ret(ladoA, ladoB):
     area = ladoA * ladoB
+    return area
+
+def area_circ(diametro):
+    area = math.pi * (diametro ** 2) / 4 
     return area
